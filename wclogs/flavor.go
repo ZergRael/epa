4fe03d916a0796718ca91942d1dda7783e1cfd6a
zergrael <zergrael@gmail.com>
2022-11-12 23:17:45 +0100

    Simplify flavor enum and prepare expansions fetch

--- wclogs/flavor.go
+++ wclogs/flavor.go
@@ -1,5 +1,10 @@
 package wclogs
 
+import (
+	"context"
+	"github.com/machinebox/graphql"
+)
+
 const (
 	retailApiUri  = "https://www.warcraftlogs.com/api/v2/client"
 	classicApiUri = "https://classic.warcraftlogs.com/api/v2/client"
@@ -12,24 +17,18 @@ type Flavor int
 const (
 	Retail Flavor = iota
 	Classic
-	TBC
-	WOTLK
 	Vanilla
 )
 
 // String returns printable Flavor
 func (f Flavor) String() string {
-	return [...]string{"Retail", "Classic", "TBC", "WOTLK", "Vanilla"}[f]
+	return [...]string{"Retail", "Classic", "Vanilla"}[f]
 }
 
 func (f Flavor) Uri() string {
 	uri := retailApiUri
 	switch f {
 	case Classic:
-		fallthrough
-	case TBC:
-		fallthrough
-	case WOTLK:
 		uri = classicApiUri
 	case Vanilla:
 		uri = vanillaApiUri
@@ -41,5 +40,34 @@ func (f Flavor) Uri() string {
 // Expansion returns the current expansion ID for a Flavor
 // TODO: This really shouldn't be hardcoded
 func (f Flavor) Expansion() int {
-	return [...]int{4, 1000, 1001, 1002, 2000}[f]
+	return [...]int{4, 1002, 2000}[f]
+}
+
+// getLatestExpansion the latest expansion ID for a specific Flavor
+func (w *WCLogs) getLatestExpansion() (int, error) {
+	req := graphql.NewRequest(`
+    query {
+		worldData {
+			expansions {
+				id
+				name
+			}
+		}
+    }
+`)
+
+	var resp struct {
+		WorldData struct {
+			Expansions []struct {
+				ID   int
+				Name string
+			}
+		}
+	}
+
+	if err := w.client.Run(context.Background(), req, &resp); err != nil {
+		return 0, err
+	}
+
+	return resp.WorldData.Expansions[0].ID, nil
 }
